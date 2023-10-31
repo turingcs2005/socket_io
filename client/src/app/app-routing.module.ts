e39017160c49aab511e9743e113c5a548f55c6c2
Rui Wang <rui.wang.624@gmail.com>
2023-10-31 10:34:14 -0400

    removed files

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HelpComponent } from './components/help/help.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'help', component: HelpComponent },
-  { path: 'socket_io', loadChildren: () => import('./modules/socket-io/socket-io.module').then(m => m.SocketIoModule) },
   { path: 'chat', loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule) }
 ];
 
